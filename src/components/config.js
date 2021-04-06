3042f9f9b8a4a2eccd1f7b71a7884d0ba25ed88f
tyr924 <120090203@link.cuhk.edu.cn>
2021-04-06 16:43:48 +0800

    feat：将奖品修改为午餐选择

--- src/components/config.js
+++ src/components/config.js
@@ -1,42 +1,42 @@
 export const prizeList = [
   {
     icon: require("../assets/img/bean_500.png"), // 奖品图片
-    name: "自助", // 奖品名称
+    name: "南昆山肠粉", // 奖品名称
     isPrize: 1 // 该奖项是否为奖品
   },
   {
     icon: require("../assets/img/bean_five.png"),
-    name: "日料",
+    name: "重庆小面",
     isPrize: 1
   },
   {
     icon: require("../assets/img/bean_one.png"),
-    name: "烤肉烤串",
+    name: "石磨肠粉",
     isPrize: 1
   },
   {
     icon: require("../assets/img/point_five.png"),
-    name: "东南亚菜",
+    name: "黄焖鸡",
     isPrize: 1
   },
   {
     icon: require("../assets/img/point_ten.png"),
-    name: "西餐",
+    name: "煎饼",
     isPrize: 1
   },
   {
     icon: require("../assets/img/bean_500.png"),
-    name: "韩料",
+    name: "首席日式拉面",
     isPrize: 1
   },
   {
     icon: require("../assets/img/bean_one.png"),
-    name: "火锅",
+    name: "新鲜事物",
     isPrize: 1
   },
   {
     icon: require("../assets/img/bean_500.png"),
-    name: "食堂",
+    name: "食堂或者全家福",
     isPrize: 1
   }
 ]
